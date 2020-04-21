2cc624717d2daeb8367be12c40c9e6c73ec700d3
Masanobu Jimbo <j.kam.j.kan.w.jmj@gmail.com>
2020-04-21 20:53:57 +0900

    :98

---
+++ lib/ModInt.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+const ll MOD = 11;
+
+class mint{
+    ll n;
+public:
+    mint(ll m = 0){
+        n = ((m % MOD) + MOD) % MOD;
+    }
+    mint operator-() const {
+        return mint(-n);
+    }
+    mint& operator+=(const mint& m){
+        n += m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint& m){
+        n += MOD - m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator*=(const  mint& m){
+        n *= m.n;
+        n %= MOD;
+        return *this;
+    }
+    mint operator+(const mint& m) const{
+        mint res(*this);
+        res += m;
+        return res;
+    }
+    mint operator-(const mint& m) const{
+        mint res(*this);
+        res -= m;
+        return res;
+    }
+    mint operator*(const mint& m) const{
+        mint res(*this);
+        res *= m;
+        return res;
+    }
+    mint pow(ll t) const{
+        if(t == 0) return mint(1);
+        mint m = pow(t>>1);
+        m *= m;
+        if(t & 1 == 1) m *= *this;
+        return m;
+    }
+    // for prime modulo
+    mint inv() const{
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint& m){
+        (*this) *= m.inv();
+        n %= MOD;
+        return *this;
+    }
+    mint operator/(const mint& m) const{
+        mint res(*this);
+        res /= m;
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.n;
+        return os;
+    }
+};
+
+
+int main(){
+    mint a(5);
+    mint b(7);
+    mint c(3);
+    cout << "MOD = " << MOD << endl; 
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / c = " << a / c << endl;
+    a += c;
+    cout << "(a += c) = " << a << endl;
+    a /= b;
+    cout << "(a /= b) = " << a << endl;
+}
---
+++ training/beginner/98.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const ll MOD = 1e9+7;
+
+class mint{
+    ll n;
+public:
+    mint(ll m = 0){
+        n = ((m % MOD) + MOD) % MOD;
+    }
+    mint operator-() const {
+        return mint(-n);
+    }
+    mint& operator+=(const mint& m){
+        n += m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint& m){
+        n += MOD - m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator*=(const  mint& m){
+        n *= m.n;
+        n %= MOD;
+        return *this;
+    }
+    mint operator+(const mint& m) const{
+        mint res(*this);
+        res += m;
+        return res;
+    }
+    mint operator-(const mint& m) const{
+        mint res(*this);
+        res -= m;
+        return res;
+    }
+    mint operator*(const mint& m) const{
+        mint res(*this);
+        res *= m;
+        return res;
+    }
+    mint pow(ll t) const{
+        if(t == 0) return mint(1);
+        mint m = pow(t>>1);
+        m *= m;
+        if(t & 1 == 1) m *= *this;
+        return m;
+    }
+    // for prime modulo
+    mint inv() const{
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint& m){
+        (*this) *= m.inv();
+        n %= MOD;
+        return *this;
+    }
+    mint operator/(const mint& m) const{
+        mint res(*this);
+        res /= m;
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.n;
+        return os;
+    }
+};
+
+ll N;
+ll c[3];
+ll A[100000];
+
+int main(){
+    cin >> N;
+    for(int i=0;i<N;i++) cin >> A[i];
+    for(int i=0;i<3;i++) c[i] = 0;
+    mint ans(1);
+    for(int i=0;i<N;i++){
+        int count = 0;
+        int index = 0;
+        for(int j=0;j<3;j++){
+            if(c[j] == A[i]){
+                count++;
+                index = j;
+            }
+        }
+        ans *= mint(count);
+        c[index]++;
+    }
+    cout << ans << endl;
+    return 0;
+}
