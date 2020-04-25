898138c1832bd4589cfb5e97010739d918a5281a
Masanobu Jimbo <j.kam.j.kan.w.jmj@gmail.com>
2020-04-25 11:49:59 +0900

    65,74

--- training/beginner/65.cpp
+++ training/beginner/65.cpp
@@ -2,42 +2,43 @@
 
 using namespace std;
 typedef long long ll;
-
-ll N,M,K;
-unordered_map<ll,ll> um[100001];
-ll cost[100001];
-vector<ll> v;
-priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
-ll INF = 1e12;
+ll N;
+ll x[100001];
+ll y[100001];
+unordered_map<ll,ll> ums[100001];
+ll z[100001];
+priority_queue<pair<ll,ll>, vector<pair<ll,ll>> ,greater<pair<ll,ll>>> pq;
 
 int main(){
-    cin >> N >> M >> K;
-    for(int i=0;i<M;i++){
-        ll a,b,c;
-        cin >> a >> b >> c;
-        um[a-1][b-1] = c;
-        um[b-1][a-1] = c;
+    cin >> N;
+    for(int i=0;i<N;i++) cin >> x[i] >> y[i];
+    vector<pair<ll,ll>> vx,vy;
+    for(int i=0;i<N;i++){
+        vx.push_back(make_pair(x[i],i));
+        vy.push_back(make_pair(y[i],i));
     }
-    for(int i=0;i<N;i++) cost[i] = INF;
-    pq.push(make_pair(0,0));
+    sort(vx.begin(),vx.end());
+    sort(vy.begin(),vy.end());
+    for(int i=1;i<N;i++){
+        ums[vx[i].second][vx[i-1].second] = ums[vx[i-1].second][vx[i].second] = min(abs(x[vx[i].second] - x[vx[i-1].second]), abs(y[vx[i].second] - y[vx[i-1].second]));
+        ums[vy[i].second][vy[i-1].second] = ums[vy[i-1].second][vy[i].second] = min(abs(y[vy[i].second] - y[vy[i-1].second]), abs(x[vy[i].second] - x[vy[i-1].second]));
+    }
+    for(int i=0;i<N;i++) z[i] = 0;
+    z[0] = 1;
+    for(auto p=ums[0].begin(); p!=ums[0].end(); p++){
+        pq.push(make_pair(p->second, p->first));
+    }
+    ll cost = 0;
     while(!pq.empty()){
-        pair<ll,ll> p = pq.top();
+        pair<ll,ll> pp = pq.top();
         pq.pop();
-        ll c = p.first;
-        ll n = p.second;
-        if(cost[n] != INF) continue;
-        cost[n] = c;
-        if(c > 0) v.push_back(c);
-        for(auto &e : um[n]){
-            ll ep = e.first;
-            ll l = e.second;
-            if(cost[ep] != INF) continue;
-            pq.push(make_pair(l,ep));
+        if(z[pp.second] != 0) continue;
+        z[pp.second] = 1;
+        cost += pp.first;
+        for(auto p=ums[pp.second].begin(); p!=ums[pp.second].end(); p++){
+            if(z[p->first] == 0) pq.push(make_pair(p->second, p->first));
         }
     }
-    sort(v.begin(),v.end());
-    ll sum = 0;
-    for(int i=0;i<(v.size()-K+1);i++) sum += v[i];
-    cout << sum << endl;
+    cout << cost << endl;
     return 0;
 }
---
+++ training/beginner/74.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll MOD = 1e9+7;
+
+class mint{
+    ll n;
+public:
+    mint(ll m = 0){
+        n = ((m % MOD) + MOD) % MOD;
+    }
+    mint operator-() const {
+        return mint(-n);
+    }
+    mint& operator+=(const mint& m){
+        n += m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint& m){
+        n += MOD - m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator*=(const  mint& m){
+        n *= m.n;
+        n %= MOD;
+        return *this;
+    }
+    mint operator+(const mint& m) const{
+        mint res(*this);
+        res += m;
+        return res;
+    }
+    mint operator-(const mint& m) const{
+        mint res(*this);
+        res -= m;
+        return res;
+    }
+    mint operator*(const mint& m) const{
+        mint res(*this);
+        res *= m;
+        return res;
+    }
+    mint pow(ll t) const{
+        if(t == 0) return mint(1);
+        mint m = pow(t>>1);
+        m *= m;
+        if(t & 1 == 1) m *= *this;
+        return m;
+    }
+    // for prime modulo
+    mint inv() const{
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint& m){
+        (*this) *= m.inv();
+        n %= MOD;
+        return *this;
+    }
+    mint operator/(const mint& m) const{
+        mint res(*this);
+        res /= m;
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.n;
+        return os;
+    }
+};
+
+ll N,K;
+
+mint fuct(ll a){
+    mint b(1);
+    for(int i=1;i<=a;i++) b *= mint(i);
+    return b;
+}
+
+mint comb(ll a, ll b){
+    mint c,d,e;
+    c = fuct(a);
+    d = fuct(b);
+    e = fuct(a-b);
+    return c / d / e;
+}
+
+int main(){
+    cin >> N >> K;
+    cout << comb(N+K-1,K) << endl;
+    return 0;
+}
