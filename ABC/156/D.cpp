e60b830ba2064356755853ecaf83358472bc233b
Masanobu Jimbo <j.kam.j.kan.w.jmj@gmail.com>
2020-05-10 15:09:19 +0900

    156D

---
+++ ABC/156/D.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll MOD = 1e9+7;
+
+class mint{
+    ll n;
+public:
+    mint(ll m = 0){
+        n = ((m % MOD) + MOD) % MOD;
+    }
+    mint operator-() const {
+        return mint(-n);
+    }
+    mint& operator+=(const mint& m){
+        n += m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint& m){
+        n += MOD - m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator*=(const  mint& m){
+        n *= m.n;
+        n %= MOD;
+        return *this;
+    }
+    mint operator+(const mint& m) const{
+        mint res(*this);
+        res += m;
+        return res;
+    }
+    mint operator-(const mint& m) const{
+        mint res(*this);
+        res -= m;
+        return res;
+    }
+    mint operator*(const mint& m) const{
+        mint res(*this);
+        res *= m;
+        return res;
+    }
+    mint pow(ll t) const{
+        if(t == 0) return mint(1);
+        mint m = pow(t>>1);
+        m *= m;
+        if(t & 1 == 1) m *= *this;
+        return m;
+    }
+    // for prime modulo
+    mint inv() const{
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint& m){
+        (*this) *= m.inv();
+        n %= MOD;
+        return *this;
+    }
+    mint operator/(const mint& m) const{
+        mint res(*this);
+        res /= m;
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.n;
+        return os;
+    }
+};
+
+ll n,a,b;
+
+mint comb(ll s, ll t){
+    mint c = mint(1);
+    mint d = mint(1);
+    for(int i=0;i<t;i++){
+        c *= mint(s - (ll)i);
+        d *= mint((ll)i + 1);
+    }
+    return c / d;
+}
+
+int main(){
+    cin >> n >> a >> b;
+    mint x = mint(2);
+    x = x.pow(n);
+    x -= mint(1);
+    mint y = comb(n,a);
+    mint z = comb(n,b);
+    cout << x - y - z << endl;
+    return 0;
+}
