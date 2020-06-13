386eaf4acd2029fbc076ce0678a77a5c87853085
Masanobu Jimbo <j.kam.j.kan.w.jmj@gmail.com>
2020-06-13 16:56:14 +0900

    ok

--- ABC/162/e.cpp
+++ ABC/162/e.cpp
@@ -3,7 +3,87 @@
 using namespace std;
 typedef long long ll;
 
+ll MOD = 1e9+7;
+
+class mint{
+    ll n;
+public:
+    mint(ll m = 0){
+        n = ((m % MOD) + MOD) % MOD;
+    }
+    mint operator-() const {
+        return mint(-n);
+    }
+    mint& operator+=(const mint& m){
+        n += m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint& m){
+        n += MOD - m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator*=(const  mint& m){
+        n *= m.n;
+        n %= MOD;
+        return *this;
+    }
+    mint operator+(const mint& m) const{
+        mint res(*this);
+        res += m;
+        return res;
+    }
+    mint operator-(const mint& m) const{
+        mint res(*this);
+        res -= m;
+        return res;
+    }
+    mint operator*(const mint& m) const{
+        mint res(*this);
+        res *= m;
+        return res;
+    }
+    mint pow(ll t) const{
+        if(t == 0) return mint(1);
+        mint m = pow(t>>1);
+        m *= m;
+        if(t & 1 == 1) m *= *this;
+        return m;
+    }
+    // for prime modulo
+    mint inv() const{
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint& m){
+        (*this) *= m.inv();
+        n %= MOD;
+        return *this;
+    }
+    mint operator/(const mint& m) const{
+        mint res(*this);
+        res /= m;
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.n;
+        return os;
+    }
+};
+
+ll N,K;
+mint c[100001];
+
 int main(){
-    
+    cin >> N >> K;
+    for(int i=K;i>=1;i--){
+        for(int j=2;i*j<=K;j++){
+            c[i] -= c[i*j];
+        }
+        c[i] += mint(K/i).pow(N);
+    }
+    mint ans(0);
+    for(int i=1;i<=K;i++) ans += c[i] * mint(i);
+    cout << ans << endl;
     return 0;
 }
