32f4ba6fafe39fd76d8743a258e0bfa3b4c7bdc0
Masanobu Jimbo <j.kam.j.kan.w.jmj@gmail.com>
2020-06-18 21:21:35 +0900

    ok

---
+++ ARC/034/C.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> P;
+
+ll MOD = 1e9+7;
+
+class mint{
+    ll n;
+public:
+    mint(ll m = 0){
+        n = ((m % MOD) + MOD) % MOD;
+    }
+    mint operator-() const {
+        return mint(-n);
+    }
+    mint& operator+=(const mint& m){
+        n += m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator-=(const mint& m){
+        n += MOD - m.n;
+        if(n >= MOD) n -= MOD;
+        return *this;
+    }
+    mint& operator*=(const  mint& m){
+        n *= m.n;
+        n %= MOD;
+        return *this;
+    }
+    mint operator+(const mint& m) const{
+        mint res(*this);
+        res += m;
+        return res;
+    }
+    mint operator-(const mint& m) const{
+        mint res(*this);
+        res -= m;
+        return res;
+    }
+    mint operator*(const mint& m) const{
+        mint res(*this);
+        res *= m;
+        return res;
+    }
+    mint pow(ll t) const{
+        if(t == 0) return mint(1);
+        mint m = pow(t>>1);
+        m *= m;
+        if(t & 1 == 1) m *= *this;
+        return m;
+    }
+    // for prime modulo
+    mint inv() const{
+        return pow(MOD-2);
+    }
+    mint& operator/=(const mint& m){
+        (*this) *= m.inv();
+        n %= MOD;
+        return *this;
+    }
+    mint operator/(const mint& m) const{
+        mint res(*this);
+        res /= m;
+        return res;
+    }
+    friend ostream& operator<<(ostream& os, const mint& m){
+        os << m.n;
+        return os;
+    }
+};
+
+ll A,B;
+unordered_map<ll,ll> um;
+
+int main(){
+    cin >> A >> B;
+    for(ll n=B+1;n<=A;n++){
+        ll N = n;
+        for(ll t=2;t*t<=n;t++){
+            ll c = 0;
+            while(N % t == 0){
+                N /= t;
+                c++;
+            }
+            if(c > 0) um[t] += c;
+        }
+        if(N != 1) um[N]++;
+    }
+    mint ans = 1;
+    for(auto &e : um){
+        ll a = e.first;
+        ll b = e.second;
+        ans *= mint(b+1);
+    }
+    cout << ans << endl;
+    return 0;
+}
